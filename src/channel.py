bb60ea84251cc76bf7ca407c118f45cd268ee067
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-08-16 22:25:08 +0700

    modify __init__ method, add channel_id with @property decorator, add get_service, to_json methods

--- src/channel.py
+++ src/channel.py
@@ -3,17 +3,46 @@ import os
 from googleapiclient.discovery import build
 
 api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
     """Класс для ютуб-канала"""
 
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        self.channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        # youtube = build('youtube', 'v3', developerKey=api_key)
+        self.channel = Channel.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel['items'][0]['snippet']['title']
+        self.description = self.channel['items'][0]['snippet']['description']
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
+        self.subscriber_count = self.channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel['items'][0]['statistics']['videoCount']
+        self.view_count = self.channel['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(json.dumps(self.channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return Channel.youtube
+
+    def to_json(self, file_name):
+        attr_list = [
+            self.__channel_id,
+            self.title,
+            self.description,
+            self.url,
+            self.subscriber_count,
+            self.video_count,
+            self.view_count
+        ]
+        with open(file_name, "w+") as f:
+            json.dump(attr_list, f, ensure_ascii=False)

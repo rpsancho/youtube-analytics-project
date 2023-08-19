0e373135a7471b2e4de1d024044be918e39120c4
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-08-19 17:13:15 +0700

    improve print_info method; remove channel_info from class attributes

--- src/channel.py
+++ src/channel.py
@@ -14,16 +14,16 @@ class Channel:
         self.__init_from_api()
 
     def __init_from_api(self):
-        self.channel_info = self.get_service().channels().list(
+        channel_info = self.get_service().channels().list(
             id=self.__channel_id,
             part='snippet,statistics'
         ).execute()
-        self.title = self.channel_info['items'][0]['snippet']['title']
-        self.description = self.channel_info['items'][0]['snippet']['description']
+        self.title = channel_info['items'][0]['snippet']['title']
+        self.description = channel_info['items'][0]['snippet']['description']
         self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
-        self.subscriber_count = self.channel_info['items'][0]['statistics']['subscriberCount']
-        self.video_count = self.channel_info['items'][0]['statistics']['videoCount']
-        self.view_count = self.channel_info['items'][0]['statistics']['viewCount']
+        self.subscriber_count = channel_info['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel_info['items'][0]['statistics']['videoCount']
+        self.view_count = channel_info['items'][0]['statistics']['viewCount']
 
     @property
     def channel_id(self):
@@ -31,7 +31,12 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.channel_info, indent=2, ensure_ascii=False))
+        service = self.get_service()
+        channel_info = service.channels().list(
+            id=self.__channel_id,
+            part='snippet,statistics'
+        ).execute()
+        print(json.dumps(channel_info, indent=2, ensure_ascii=False))
 
     @classmethod
     def get_service(cls):

808293bb7b0ac64270b967c5b20e7c7f16b4f9cc
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-09-02 15:15:06 +0700

    create total_duration, show_best_video, get_video_list functions

--- src/playlist.py
+++ src/playlist.py
@@ -1,5 +1,5 @@
-from datetime import datetime
 import isodate
+from src.video import Video
 from src.youtube_mixin import YoutubeMixin
 
 
@@ -7,43 +7,59 @@ class PlayList(YoutubeMixin):
 
     def __init__(self, playlist_id):
         super().__init__()
-        self.playlist_id = playlist_id
+        self.__playlist_id = playlist_id
         self.__init_from_api()
 
     def __init_from_api(self):
         playlist_info = self.get_service().playlists().list(
-            id=self.playlist_id,
+            id=self.__playlist_id,
             part='snippet',
             maxResults=50,
         ).execute()
         self.title = playlist_info['items'][0]['snippet']['title']
-        self.url = f'https://www.youtube.com/playlist?list={self.playlist_id}'
+        self.url = f'https://www.youtube.com/playlist?list={self.__playlist_id}'
 
     @property
     def total_duration(self):
-        playlist_videos = self.get_service().playlistItems().list(
-            playlistId=self.playlist_id,
-            part='contentDetails',
-            maxResults=50,
-        ).execute()
-        video_ids: list[str] = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+
+        video_ids: list[str] = self.get_video_list()
+
         video_response = self.get_service().videos().list(
             part='contentDetails,statistics',
             id=','.join(video_ids)
         ).execute()
-        duration = 0
+
+        duration = isodate.duration.timedelta(0, 0, 0, 0, 0, 0, 0)
+
         for video in video_response['items']:
-            iso_duration = isodate.parse_duration(video['contentDetails']['duration'])
-            duration += datetime.fromisoformat(iso_duration)
+            iso_dur = isodate.parse_duration(video['contentDetails']['duration'])
+            duration += iso_dur
         return duration
 
-    # @total_duration.setter
-    # def total_duration(self, response):
-    #     for video in response['items']:
-    #         # YouTube video duration is in ISO 8601 format
-    #         iso_8601_duration = video['contentDetails']['duration']
-    #         # duration = isodate.parse_duration(iso_8601_duration)
-    #     return self.duration
-
     def show_best_video(self):
-        pass
+
+        video_ids: list[str] = self.get_video_list()
+
+        like_count_max = 0
+        best_video_id = ''
+
+        for video_id in video_ids:
+            video = Video(video_id)
+            like_count = int(video.like_count)
+            if like_count > like_count_max:
+                like_count_max = like_count
+                best_video_id = video.video_id
+        best_video = Video(best_video_id)
+        return best_video.url
+
+    def get_video_list(self):
+
+        playlist_videos = self.get_service().playlistItems().list(
+            playlistId=self.__playlist_id,
+            part='contentDetails',
+            maxResults=50,
+        ).execute()
+
+        video_ids = [video['contentDetails']['videoId'] for video in playlist_videos['items']]
+
+        return video_ids

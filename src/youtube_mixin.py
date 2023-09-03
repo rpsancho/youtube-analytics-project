7c22c2ca86d8a406eafc2517db428db83a74b33c
Alexander Polikarpov <alex-polikarpov@yandex.ru>
2023-09-03 10:08:34 +0700

    remove unused __init__

--- src/youtube_mixin.py
+++ src/youtube_mixin.py
@@ -6,9 +6,6 @@ class YoutubeMixin:
 
     api_key: str = os.getenv('YT_API_KEY')
 
-    def __init__(self):
-        pass
-
     @classmethod
     def get_service(cls):
         service = build('youtube', 'v3', developerKey=cls.api_key)
